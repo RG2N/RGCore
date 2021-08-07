674c8a810c245f8a6f0e508d4811281a54ba0d36
RG2N <64289295+RG2N@users.noreply.github.com>
2021-08-07 15:26:41 -0400

    Add log function.

--- src/me/RG2N/RGCore/Main.java
+++ src/me/RG2N/RGCore/Main.java
@@ -2,12 +2,18 @@ package me.RG2N.RGCore;
 
 public class Main {
 	
+	String prefix = "[RGCore] ";
+	
 	public void onEnable() {
 		
 	}
 	
 	public void onDisable() {
 		
+	}
+	
+	public void log(String msg) {
+		System.out.println(prefix + msg);
 	}
 
 }
